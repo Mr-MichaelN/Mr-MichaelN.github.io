99b8fc687313013b4cfe6de19453deaccd9b7a2d
Nwarienne Michael Chukwudi <jamaicaspecie@gmail.com>
2023-11-18 11:35:56 +0100

    udated files

--- js/theme.js
+++ js/theme.js
@@ -1,4 +1,23 @@
 'use strict';
+// Function to update the current time
+function updateTime() {
+    // Get the current date and time
+    const now = new Date();
+
+    // Format the time as per your requirement
+    const hours = now.getHours().toString().padStart(2, '0');
+    const minutes = now.getMinutes().toString().padStart(2, '0');
+    const seconds = now.getSeconds().toString().padStart(2, '0');
+
+    // Display the time in a specific element (replace 'timeDisplay' with the actual ID of the element)
+    document.getElementById('timeDisplay').innerText = `${hours}:${minutes}:${seconds}`;
+
+    // Update the time every second
+    setTimeout(updateTime, 1000);
+}
+
+// Call the function when the document is fully loaded
+document.addEventListener('DOMContentLoaded', updateTime);
 document.addEventListener('DOMContentLoaded', function () {
     /* =====================================================
         TESTIMONIALS SLIDER
@@ -60,10 +79,10 @@ document.addEventListener('DOMContentLoaded', function () {
             duration: 4,
         };
 
-        var counter1 = new CountUp('counter1', 0, 120, 0, options);
+        var counter1 = new CountUp('counter1', 0, 20, 0, options);
         var counter2 = new CountUp('counter2', 0, 50, 0, options);
-        var counter3 = new CountUp('counter3', 0, 320, 0, options);
-        var counter4 = new CountUp('counter4', 0, 333, 0, options);
+        var counter3 = new CountUp('counter3', 0, 25, 0, options);
+        var counter4 = new CountUp('counter4', 0, 100, 0, options);
 
         if (!counter1.error) {
             counter1.start();
@@ -133,34 +152,4 @@ document.addEventListener('DOMContentLoaded', function () {
         resetAnimation: true,
     });
     wow.init();
-});
-
-document.addEventListener('DOMContentLoaded', function () {
-    var introText = document.getElementById('intro-text');
-
-    var styles = [
-        { animation: 'fadeInOut 2s forwards', color: 'red' },      // Fade in/out
-        { animation: 'circular 2s forwards', color: 'blue' },       // Circular animation
-        { animation: 'dissolve 2s forwards', color: 'green' },       // Dissolve animation
-        { animation: 'wipeDown 2s forwards', color: 'orange' },       // Wipe down
-        { animation: 'wipeLeft 2s forwards', color: 'purple' },       // Wipe left
-        { animation: 'wipeRight 2s forwards', color: 'yellow' },      // Wipe right
-        { animation: 'wipeUp 2s forwards', color: 'pink' },         // Wipe up
-        { animation: 'cutThroughBack 2s forwards', color: 'brown' }, // Cut through back
-        { animation: 'cut 2s forwards', color: 'cyan' },            // Cut
-        { animation: 'fadeThroughBack 2s forwards', color: 'magenta' },// Fade through back
-        { animation: 'fadeSmoothly 2s forwards', color: 'teal' }     // Fade smoothly
-        // Add more styles with different animation effects and colors
-    ];
-
-    var currentIndex = 0;
-
-    function changeStyle() {
-        Object.assign(introText.style, styles[currentIndex]);
-        currentIndex = (currentIndex + 1) % styles.length;
-    }
-
-    changeStyle(); // Initially change the style
-
-    setInterval(changeStyle, 8000); // Set an interval to change the style every 8 seconds
 });
